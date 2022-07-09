a2c0c7471e2c58b830cf8375a3e676fceab9903e
Reece Williams <reecepbcups@gmail.com>
2022-07-09 13:33:56 -0500

    state.rs

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # CosmWasm Starter Pack
 ## zero-to-hero-sparkibc
 
-notes
+### Polling APP like starpoll on chain 
+
+
+**notes**
 ```
 src/ folder (template generated for you)
 
@@ -9,9 +12,9 @@ lib.rs - defines files in the project & exports them for use
 contract.rs - where actual contract logic happens (uses messages)
 error.rs - contract errors for when logic does not line up with what you want
 
-helpers.rs - we deleted this as it is not needed, alkso removed from lib.rs
+helpers.rs - we deleted this as it is not needed, also removed from lib.rs
 
-msg.rs - 
+msg.rs - holds messages which we use in the project & has more info in there.
 
 state.rs - storage of the contract via Maps / Key Value Stores
 ```
--- src/state.rs
+++ src/state.rs
@@ -4,14 +4,17 @@ use serde::{Deserialize, Serialize};
 use cosmwasm_std::Addr;
 use cw_storage_plus::Item;
 
-
+// we rename State -> Config bc it can get confusing with this being state.rs
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct State {
-    pub count: i32,
-    pub owner: Addr,
+pub struct Config {
+    // pub = public, Addr is a wallet address (like juno10r39fueph9fq7a6lgswu4zdsg8t3gxlq670lt0)
+    pub admin_address: Addr,
 }
+
+// we can get the data like config.admint_address
 
 // an Item of State which is in the storage_key "state".
 // This key HAS to be unique like a KV Store
-pub const STATE: Item<State> = Item::new("state");
+// CONFIG is all caps = Constant. So in storage it will be prefixed with "config" for the section
+pub const CONFIG: Item<Config> = Item::new("config");
