9df2a0a21ae8fc6a8d1b5b8f1f22ad729d5799b2
Reece Williams <reecepbcups@gmail.com>
2022-07-09 13:40:59 -0500

    version

--- src/contract.rs
+++ src/contract.rs
@@ -1,15 +1,13 @@
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
 use cosmwasm_std::{Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
-// use cw2::set_contract_version;
+use cw2::set_contract_version; // cw2 is a spec which lets users have contract metadata (name, version)
 
 use crate::error::ContractError;
 use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 
-/*
 const CONTRACT_NAME: &str = "crates.io:zero-to-hero-discord";
-const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
- */
+const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION"); // Config.toml -> [package] -> version
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn instantiate(
